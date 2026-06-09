d13e060f813ecbe32df4cde84f81786c40332a69
evokelektrique <majidmohseni3412@gmail.com>
2026-06-09 18:52:54 +0330

    android: disable APK dependency metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -114,6 +114,11 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
